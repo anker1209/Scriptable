e1a448673604468d570541b05572436776690b40
脑瓜 <76637082+anker1209@users.noreply.github.com>
2021-02-01 00:51:33 +0800

    Update JD-in-one.js

--- scripts/JD-in-one.js
+++ scripts/JD-in-one.js
@@ -5,7 +5,7 @@
 // 电报群: https://t.me/Scriptable_JS @anker1209
 // 采用了2Ya美女的京豆收支脚本及DMYY依赖 https://github.com/dompling/Scriptable/tree/master/Scripts
 // version:1.1.1
-// updata:2021/01/27
+// updata:2021/02/01
 let widgetParam = args.widgetParameter;
 let cookie ='';
 let userID = decodeURIComponent(cookie.match(/pt_pin=(.+?);/) && cookie.match(/pt_pin=(.+?);/)[1]);
